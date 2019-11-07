cf605bf13da9b8bdbc9439ae8fbea63f7f294e19
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-07 13:44:01 -0500

    Add files via upload

--- arreglosDinamicos.cpp
+++ arreglosDinamicos.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
-using namespace std;
-int factorial (int n);
-int main(int argc, char const *argv[]) {
-  int numero = atoi(argv[1]);
+#include <stdlib.h>
 
-  cout<<"Factorial de "<<factorial (numero) << endl;
+using namespace std;
+
+int factorial(int n);
+
+int main(int argc, char const *argv[]) {
+
+  int n;
+  cout << "ingrese n" << endl;
+  cin >> n;
+
+  int *array1 = new int [n];
+
+  for (size_t i = 0; i < n; i++) {
+    array1[i] = factorial(i);
+  }
+
+  for (size_t j = 0; j < n; j++) {
+    cout << "array[" << j << "]= " << array1[j] << endl;
+  }
 
   return 0;
 }
-int factorial (int n){
-
-  if (n >= 1){
-    return n*factorial(n-1);;
-  }
-  else {
+int factorial(int n){
+  if(n == 0){
     return 1;
   }
-    //cout<<"Factorial de "<< factorial(n) << endl;
+  else{
+    return n*factorial(n-1);
+  }
 }
