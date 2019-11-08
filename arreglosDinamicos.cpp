6591b93502a6d92460ab2d27546c96b5de3afe92
AndresPerez07 <56890923+AndresPerez07@users.noreply.github.com>
2019-11-07 22:48:22 -0500

    Add files via upload

--- arreglosDinamicos.cpp
+++ arreglosDinamicos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 
 using namespace std;
 
 int factorial(int n);
+float *multiplica(float *x, float *y, int N);
 
 int main(int argc, char const *argv[]) {
 
@@ -18,9 +20,49 @@ int main(int argc, char const *argv[]) {
   }
 
   for (size_t j = 0; j < n; j++) {
-    cout << "array[" << j << "]= " << array1[j] << endl;
+    cout << "factoriales[" << j << "]= " << array1[j] << endl;
   }
 
+  ifstream file1;
+  ifstream file2;
+
+  file1.open("valores_x.txt");
+  file2.open("valores_y.txt");
+  float plaka;
+  int N=0;
+  while(file1 >> plaka){
+      N ++;
+  }
+
+  file1.clear();
+  file1.seekg(0);
+
+  float *x = new float [N];
+  float *y = new float [N];
+  float *mult = new float [N];
+
+  for (size_t i = 0; i < N; i++) {
+    file1 >> x[i];
+    file2 >> y[i];
+  }
+
+  for (size_t j = 0; j < N; j++) {
+    cout << "x[" << j << "]= " << x[j] << " , " << "y[" << j << "]= " << y[j] << endl;
+  }
+
+  mult = multiplica(x,y,N);
+
+  for (size_t j = 0; j < N; j++) {
+    cout << "x*y[" << j << "]= " << mult[j] << endl;
+  }
+
+  file1.close();
+  file2.close();
+
+  delete[] array1;
+  delete[] x;
+  delete[] y;
+  delete[] mult;
   return 0;
 }
 int factorial(int n){
@@ -30,4 +72,14 @@ int factorial(int n){
   else{
     return n*factorial(n-1);
   }
+}
+float *multiplica(float *x, float *y, int N){
+
+  float *m = new float [N];
+
+  for (size_t i = 0; i < N; i++) {
+    m[i] = x[i]*y[i];
+  }
+
+  return m;
 }
